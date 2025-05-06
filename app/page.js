ef34c906a9d005c6404f4486190d92b3aa60da93
Kuldep Sahoo <sahookuldeep192@gmail.com>
2025-05-06 20:54:49 +0530

    third

--- app/page.js
+++ app/page.js
@@ -170,7 +170,7 @@ export default function QRApp() {
               </div>
               <div>
                 <button
-                  onClick={reset}
+                  onClick={() => { reset(); startScanner();}}
                   className="bg-red-600 text-white px-4 py-2 rounded hover:bg-teal-700 transition"
                 >
                   Reset Cam
